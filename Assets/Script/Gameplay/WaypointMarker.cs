a9f1fc3737468efb21761ade995b28609d41839b
DW256 <dwakurniawan@gmail.com>
2024-11-22 23:56:58 +0700

    fix : waypoint marker
    bugged when target z < 0

--- Assets/Script/Gameplay/WaypointMarker.cs
+++ Assets/Script/Gameplay/WaypointMarker.cs
@@ -1,45 +1,59 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class WaypointMarker : MonoBehaviour
 {
-    public Image WaypointImage;
-    public Transform Target;
-    public TMP_Text DistanceText;
-    public Vector3 Offset;
+    [SerializeField] private Image waypointImage;
+    [SerializeField] private Image bar;
+    [SerializeField] private Transform target;
+    [SerializeField] private TMP_Text distanceText;
+    [SerializeField] private Vector3 offset;
     private Transform player;
 
-    private void Start() {
+    private void Start()
+    {
         player = GameObject.FindGameObjectWithTag("Player")?.transform;
     }
 
     private void Update()
     {
-        float minX = WaypointImage.GetPixelAdjustedRect().width / 2;
+        UpdateWaypointPosition();
+        UpdateDistanceText();
+    }
+
+    private void UpdateWaypointPosition()
+    {
+        float minX = waypointImage.GetPixelAdjustedRect().width / 2;
         float maxX = Screen.width - minX;
 
-        float minY = WaypointImage.GetPixelAdjustedRect().height / 2;
+        float minY = waypointImage.GetPixelAdjustedRect().height / 2;
         float maxY = Screen.height - minY;
 
-        Vector2 pos = Camera.main.WorldToScreenPoint(Target.position + Offset);
+        Vector3 screenPos = Camera.main.WorldToViewportPoint(target.position + offset);
 
-        if (Vector3.Dot((Target.position - transform.position), transform.forward) < 0)
+        if (screenPos.z < 0)
         {
-            if (pos.x < Screen.width / 2)
-            {
-                pos.x = maxX;
-            }
-            else
-            {
-                pos.x = minX;
-            }
+            screenPos.x = 1f - screenPos.x;
+            screenPos.y = 1f - screenPos.y;
+            screenPos.z = 0;
         }
 
+        Vector2 pos = Camera.main.ViewportToScreenPoint(new Vector2(screenPos.x, screenPos.y));
         pos.x = Mathf.Clamp(pos.x, minX, maxX);
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
 
-        WaypointImage.transform.position = pos;
-        DistanceText.text = ((int)Vector3.Distance(Target.position, player.position)).ToString() + "m";
+        waypointImage.transform.position = pos;
+    }
+
+    private void UpdateDistanceText()
+    {
+        distanceText.text = ((int)Vector3.Distance(target.position, player.position)).ToString() + "m";
+    }
+
+    public void UpdateBar(float current, float max)
+    {
+        bar.DOFillAmount(current / max, 0.1f); // Smoothly animate the bar fill
     }
 }
